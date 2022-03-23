3343beb36d090118788495a4c6572c1a6f449f98
Sam Ritchie <sritchie09@gmail.com>
2022-03-23 17:53:52 +0000

    bugfix

--- src/demo/viewers.cljs
+++ src/demo/viewers.cljs
@@ -9,25 +9,6 @@
 (swap! sv/!sci-ctx
        sci/merge-opts
        {:namespaces
-        {'demo.mathbox
-         {'build-mathbox demo.mathbox/build-mathbox
-          'color demo.mathbox/color
-          'orbit demo.mathbox/orbit
-          'setup-scene demo.mathbox/setup-scene
-          'initialize! demo.mathbox/initialize!
-          'sync! demo.mathbox/sync!
-          'sync-once! demo.mathbox/sync-once!
-          '->cartesian-view demo.mathbox/->cartesian-view
-          'add-volume! demo.mathbox/add-volume!
-
-          'basic-setup demo.mathbox/basic-setup
-          'function-demo demo.mathbox/function-demo
-
-          'polar-setup demo.mathbox/polar-setup
-          'polar-demo demo.mathbox/polar-demo
-
-          'physics-demo demo.mathbox/physics-demo
-          'double-physics-demo demo.mathbox/double-physics-demo
-          'oscillator-demo demo.mathbox/oscillator-demo}}
+        {'demo.mathbox (sci/copy-ns demo.mathbox (sci/create-ns 'demo.mathbox))}
         :classes {'Math js/Math}
         :aliases {'mb 'demo.mathbox}})
