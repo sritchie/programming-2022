bcc4103e307bcb5ec02697c7d94d6bace930799d
Sam Ritchie <sritchie09@gmail.com>
2022-03-30 12:48:37 -0600

    commit

--- src/demo/mathbox_react.cljs
+++ src/demo/mathbox_react.cljs
@@ -1,6 +1,6 @@
 (ns demo.mathbox-react
   (:require ["mathbox-react" :as MB]
-            [reagent.core :as r]
+            [reagent.core :as r :include-macros true]
             [sicmutils.expression.compile :as xc]
             ["three/examples/jsm/controls/OrbitControls.js"
              :as OrbitControls]))
@@ -11,15 +11,6 @@
 
 (def init-cam [2.5 1 2.5])
 
-(def Mathbox (r/adapt-react-class MB/Mathbox))
-(def Cartesian (r/adapt-react-class MB/Cartesian))
-(def Axis (r/adapt-react-class MB/Axis))
-(def Grid (r/adapt-react-class MB/Grid))
-(def Volume (r/adapt-react-class MB/Volume))
-(def Interval (r/adapt-react-class MB/Interval))
-(def Point (r/adapt-react-class MB/Point))
-(def Line (r/adapt-react-class MB/Line))
-
 (def options
   {:plugins ["core" "controls" "cursor"]
    :controls {:klass orbit}
@@ -27,15 +18,15 @@
 
 (defn cube
   [{:keys [id width-rez height-rez depth-rez size opacity]}]
-  [Mathbox
+  [:> MB/Mathbox
    {:options {:plugins ["core" "controls" "cursor"]
               :controls {:klass orbit}
               :camera {}}
     :style {:height "400px" :width "100%"}
     :initialCameraPosition init-cam}
-   [Cartesian
+   [:> MB/Cartesian
     {}
-    [Volume
+    [:> MB/Volume
      {:id id
       :width width-rez
       :bufferWidth width-rez
@@ -48,7 +39,7 @@
       :live false
       :expr (fn [emit x y z]
               (emit x y z opacity))}
-     [Point
+     [:> MB/Point
       {:points (str "#" id)
        :colors (str "#" id)
        :color 0xffffff
@@ -58,26 +49,28 @@
                  :or {samples 256
                       range [[-6 10] [-1 1] [-1 1]]
                       scale [6 1 1]}}]
+  ;; TODO: extract a function, somehow, that is doing the xc/sci-eval just ONE
+  ;; time, and trying to hold back whenever the value changes.
   (let [f' (xc/sci-eval f)]
-    [Mathbox
+    [:> MB/Mathbox
      {:options
       {:plugins ["core" "controls" "cursor"]
        :controls {:klass orbit}
        :camera {}}
       :style {:height "400px" :width "100%"}
       :initialCameraPosition [2.3 1 2]}
-     [Cartesian
+     [:> MB/Cartesian
       {:range range
        :scale scale}
-      [Axis {:axis 1 :width 3}]
-      [Axis {:axis 2 :width 3}]
-      [Axis {:axis 3 :width 3}]
-      [Interval
+      [:> MB/Axis {:axis 1 :width 3}]
+      [:> MB/Axis {:axis 2 :width 3}]
+      [:> MB/Axis {:axis 3 :width 3}]
+      [:> MB/Interval
        {:width samples
         :items 1
         :channels 2
         :expr (fn [emit x _i time]
                 (let [d (f' x time)]
                   (emit x d)))}
-       [Line {:color 0x3090ff :width 4}]
-       [Point {:color 0x3090ff :size 8}]]]]))
+       [:> MB/Line {:color 0x3090ff :width 4}]
+       [:> MB/Point {:color 0x3090ff :size 8}]]]]))
--- src/reactbox.clj
+++ src/reactbox.clj
@@ -7,25 +7,44 @@
             [sicmutils.env :as e :refer :all]
             [sicmutils.expression.compile :as xc]))
 
-;; ## Hello, SICMUtils!!
-;;
-;; Is this thing on?
-
-(simplify
- (+ 'x 'x (expt 'y 4) 'x 'x))
-
-;; TODO why is reagent doing a full redreaw??
 (def cube-viewer
   {:fetch-fn (fn [_ x] x)
    :render-fn
-   '(fn [value]
+   '(fn [{:keys [id width-rez height-rez depth-rez size opacity]}]
       (v/html
-       (when value
-         [mbr/cube value])))})
+       [box/Mathbox
+        {:options {:plugins ["core" "controls" "cursor"]
+                   :controls {:klass box/OrbitControls}
+                   :camera {}}
+         :style {:height "400px" :width "100%"}
+         :initialCameraPosition [2.5 1 2.5]
+         :ref (fn [box]
+                (when box
+                  (doto (.-three ^js box)
+                    (-> .-controls .-maxDistance (set! 4))
+                    (-> .-camera .-position (.set 2.5 1 2.5))
+                    (-> .-renderer (.setClearColor (mb/color 0xeeeeee) 1.0)))))}
+        [box/Cartesian
+         {}
+         [box/Volume
+          {:id id
+           :width width-rez
+           :bufferWidth width-rez
+           :height height-rez
+           :bufferHeight height-rez
+           :depth depth-rez
+           :bufferDepth depth-rez
+           :items 1
+           :channels 4
+           :live false
+           :expr (fn [emit x y z]
+                   (emit x y z opacity))}
+          [box/Point
+           {:points (str "#" id)
+            :colors (str "#" id)
+            :color 0xffffff
+            :size size}]]]]))})
 
-;; We can then use the above viewer using `with-viewer`:
-
-;; live-changing the axes deletes everything
 (clerk/with-viewer cube-viewer
   {:id "volume"
    :width-rez 12
@@ -34,79 +53,107 @@
    :size 8
    :opacity 1.0})
 
-;; ## Function Viewer
-;;
-;; This namespace demos a viewer for function objects; these are functions that
-;; _work_ locally, you can call them in the REPL, but you can also send them
-;; over the wire to be rendered in the browser.
+;; ## Stateful Attempt
 
-;;
-;; ### Function Compilation
-;;
-;; Let's assume that the value we will send to the viewer is a map with the
-;; function we want to render stored under an `:f` keyword. How do we get the
-;; function over to the browser?
-;;
-;; We can't serialize it directly. What we _can_ do is use SICMUtil's function
-;; compiler in `:source` mode to generate a hygienic function literal.
-;;
-;; Clojure is built out of its own data structures, so that will serialize
-;; nicely.
+^{::clerk/viewer
+  '(fn [_] (defonce cube-state
+            (reagent/atom
+             {:width-rez 12
+              :height-rez 20
+              :depth-rez 22
+              :size 8
+              :opacity 1.0})))}
+(do :ignore)
 
-(defn- fn-transform [m]
-  (binding [xc/*mode* :source]
-    (update m :f #(xc/compile-fn* % 2))))
-
-;; Let's try it:
-
-(fn-transform
- {:f (fn [x _]
-       (+ (square x)
-          (cube x)))})
-
-;; Not too pretty. We can reuse `d/->pretty-str` here (and already it's obvious
-;; that a better presentation would have made `fn-transform` act on the
-;; function, not the FULL map, but whatever):
-
-(clerk/code
- (:f (fn-transform
-      {:f (fn [x _]
-            (+ (square x)
-               (cube x)))})))
-
-(def fn-render-fn
-  '(fn [value]
+^{::clerk/viewer
+  '(fn [_]
      (v/html
-      (when value
-        (mbr/function value)))))
+      [box/Mathbox
+       {:options {:plugins ["core" "controls" "cursor"]
+                  :controls {:klass box/OrbitControls}
+                  :camera {}}
+        :style {:height "400px" :width "100%"}
+        :initialCameraPosition [2.5 1 2.5]
+        :ref (fn [box]
+               (when box
+                 (doto (.-three ^js box)
+                   (-> .-controls .-maxDistance (set! 4))
+                   (-> .-camera .-position (.set 2.5 1 2.5))
+                   (-> .-renderer (.setClearColor (mb/color 0xeeeeee) 1.0)))))}
+       [box/Cartesian
+        {}
+        (let [{:keys [width-rez height-rez depth-rez size opacity]} ]
+          [box/Volume
+           {:id "volume"
+            :width (:width-rez @cube-state)
+            :bufferWidth (:width-rez @cube-state)
+            :height (:height-rez @cube-state)
+            :bufferHeight (:height-rez @cube-state)
+            :depth (:depth-rez @cube-state)
+            :bufferDepth (:depth-rez @cube-state)
+            :items 1
+            :channels 4
+            :live false
+            :expr (fn [emit x y z]
+                    (emit x y z (:opacity @cube-state)))}
+           [box/Point
+            {:points "#volume"
+             :colors "#volume"
+             :color 0xffffff
+             :size (:size @cube-state)}]])]]))}
+(do :also-ignored)
 
-;; [[fn-render-fn]] also uses some reagent state internally; this is how it's
-;; able to compare current and previous values and decide whether or not to
-;; re-render.
-
-;; The final viewer is a clojure map with these two pieces supplied:
-
-(def fn-viewer
+^{::clerk/viewer
   {:fetch-fn (fn [_ x] x)
-   :transform-fn fn-transform
-   :render-fn fn-render-fn})
+   :render-fn '#(reset! cube-state %)}}
+{:width-rez 1
+ :height-rez 20
+ :depth-rez 22
+ :size 8
+ :opacity 1.0}
 
-;; ### Demo
-;;
-;; Let's make a function to try!
 
-(defn my-fn [x t]
-  (+ (square (sin x))
-     (square
-      (cos (* t x)))))
+;; ignore this for now!
+(comment
+  (def cube-viewer
+    {:fetch-fn (fn [_ x] x)
+     :render-fn
+     '(fn [value]
+        (v/html
+         (when value
+           [mbr/cube value])))})
 
-;; The function works locally, with numbers or symbols:
+  (clerk/with-viewer cube-viewer
+    {:id "volume"
+     :width-rez 12
+     :height-rez 20
+     :depth-rez 22
+     :size 8
+     :opacity 1.0}))
 
-[(my-fn 1 2) (my-fn 'x 't)]
-
-;; Then we'll call it with our new viewer:
+;; ## Function Viewer
 
 (comment
+  (defn- fn-transform [m]
+    (binding [xc/*mode* :source]
+      (update m :f #(xc/compile-fn* % 2))))
+
+  (def fn-render-fn
+    '(fn [value]
+       (v/html
+        (when value
+          (mbr/function value)))))
+
+  (def fn-viewer
+    {:fetch-fn (fn [_ x] x)
+     :transform-fn fn-transform
+     :render-fn fn-render-fn})
+
+  (defn my-fn [x t]
+    (+ (square (sin x))
+       (square
+        (cos (* t x)))))
+
   (clerk/with-viewer fn-viewer
     {:range [[-6 10] [-1 1] [-1 1]]
      :scale [6 1 1]
